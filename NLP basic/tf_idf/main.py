3fa7601efd1ff3ee7704642b6e5fb936633c06c6
DING <48316842+404582463@users.noreply.github.com>
2021-11-13 16:55:40 +0900

    Update main.py

--- NLP basic/tf_idf/main.py
+++ NLP basic/tf_idf/main.py
@@ -1,6 +1,11 @@
 def main():
-  import  
-  sdfadsfsda
+  import  getdocuments
+  from lib import tokenization
+  from lib import tfidf
+  from lib import cosine_similarity
+
+  print("ok")
+
   return
 
 if __name__ == "__main__":
